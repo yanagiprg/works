b3239d30cb2b15cdd98e52431203320d06e282ea
yanagi_prg <takayanagi.prg@gmail.com>
2018-10-07 04:12:15 +0900

    uploading Web App

---
+++ WebApp/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  resources :users
+
+  # get '/users', to: 'users#index'
+  # get '/users/new', to: 'users#new'
+  # post '/users', to: 'users#create'
+  # get '/users/:id/edit', to: 'users#edit'
+  # patch '/users/:id', to: 'users#update'
+  # delete '/users/:id', to: 'users#destroy'
+
+end
